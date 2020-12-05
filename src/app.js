3c15969a40ee69cef25231d82f58ed047688095f
fullcycle <dfoderick@gmail.com>
2020-12-05 10:30:39 -0800

    add cors

--- src/app.js
+++ src/app.js
@@ -1,8 +1,9 @@
 var express = require("express")
+var cors = require("cors")
 var bodyParser = require("body-parser")
 var routes = require("./routes/routes.js")
 var app = express()
-
+app.use(cors)
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
