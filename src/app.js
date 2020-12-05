796d30b2dcde09370229250c06766b4f403b154f
fullcycle <dfoderick@gmail.com>
2020-12-05 11:27:10 -0800

    fix cors

--- src/app.js
+++ src/app.js
@@ -2,8 +2,9 @@ var express = require("express")
 var cors = require("cors")
 var bodyParser = require("body-parser")
 var routes = require("./routes/routes.js")
+
 var app = express()
-app.use(cors)
+app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
